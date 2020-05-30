5ff7b17cfbd15db91be21b3663ccf2e2c8471880
Jaroslaw Paradysz <jaroslaw.paradysz@gmail.com>
2020-05-30 10:30:19 +0200

    Initial commit

---
+++ PM_MVC/Controllers/UserErrorsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PM_MVC.Models;
+using PM_MVC.ViewModels;
+
+namespace PM_MVC.Controllers
+{
+    public class UserErrorsController : Controller
+    {
+        private List<UserErrorViewModel> _userErrorViewModels;
+        public UserErrorsController()
+        {
+            _userErrorViewModels = new List<UserErrorViewModel>()
+            {
+                new UserErrorViewModel()
+                {
+                    UserErrorId = Guid.NewGuid(),
+                    CreatedTime = DateTime.Now.AddDays(-1),
+                    Notes = "Test notes 1"
+                },
+                new UserErrorViewModel()
+                {
+                    UserErrorId = Guid.NewGuid(),
+                    CreatedTime = DateTime.Now.AddDays(-2),
+                    Notes = "Test notes 2"
+                },
+                new UserErrorViewModel()
+                {
+                    UserErrorId = Guid.NewGuid(),
+                    CreatedTime = DateTime.Now.AddDays(-3),
+                    Notes = "Test notes 3"
+                }
+            };
+        }
+        public IActionResult Index()
+        {
+            return View(_userErrorViewModels[0]);
+        }
+    }
+}
---
+++ PM_MVC/ViewModels/UserErrorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PM_MVC.ViewModels
+{
+    public class UserErrorViewModel
+    {
+        public Guid UserErrorId { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public string Notes { get; set; }
+    }
+}
